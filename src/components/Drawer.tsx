03fb5f311d823dbeaf1089f583139823b77ce8b8
Vitor Prata <39749209+vitorbretasprata@users.noreply.github.com>
2020-12-20 18:57:47 -0300

    Update navigation

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -2,6 +2,7 @@ import React, { memo } from "react";
 import styled from "styled-components";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
+import Home from "../screens/home";
 import List from "../screens/list";
 
 const Drawer = createDrawerNavigator();
@@ -9,6 +10,7 @@ const Drawer = createDrawerNavigator();
 export default memo(() => {
     return (
         <Drawer.Navigator>
+            <Drawer.Screen name="Home" component={Home} />
             <Drawer.Screen name="List" component={List} />
         </Drawer.Navigator>
     )
--- src/navigation.tsx
+++ src/navigation.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Home from "./screens/home";
-import List from "./screens/list";
-
 import Drawer from "./components/Drawer";
 
 const Stack = createStackNavigator();
@@ -18,10 +15,6 @@ export default function Navigation() {
     return (
       <NavigationContainer>
           <Drawer />
-          <Stack.Navigator screenOptions={NavigationOptions}>
-              <Stack.Screen name="Home" component={Home} />
-              <Stack.Screen name="List" component={List} />
-          </Stack.Navigator>
       </NavigationContainer>
     );
 }
